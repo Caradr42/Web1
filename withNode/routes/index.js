57e575315e749769612b4b6ccc6e193264ff65a6
Caradr42 <carlosAdr2gv@gmail.com>
2020-10-27 05:17:39 -0600

    finnished everything basically; added mongoose implementation with db; added content to db and how to display it on the pages

--- withNode/routes/index.js
+++ withNode/routes/index.js
@@ -1,32 +1,75 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/test');
+
+function cloneToObjArr(doc){
+    var copy = [];
+    doc.forEach(e => { 
+        if(Array.isArray(e)){
+            copy.push(e);
+        }else if (typeof e === "object"){
+            copy.push(e.toObject());
+        }
+    });
+    return copy;
+}
+
+var Schema = mongoose.Schema;
+
+var cvDataSchema = new Schema({
+    title: {type: String, required: true},
+    content: [String]
+},{collection: "cvFields"});
+
+var homeDataSchema = new Schema({
+    title: String,
+    content: [String],
+    imgURL: String
+},{collection: "homeData"});
+
+var hbDataSchema = new Schema({
+    title: String,
+    content: [String],
+    imgURL: String
+},{collection: "hbData"});
+
+var cvData = mongoose.model('cvFields', cvDataSchema);
+var homeData = mongoose.model('homeData', homeDataSchema);
+var hbData = mongoose.model('hbData', hbDataSchema);
+
+router.get('/cv', (req, res, next) => {
+    cvData.find() 
+        .then(function (doc) {
+            var copy = cloneToObjArr(doc);
+            res.render('cv', {cvFields: copy});
+        });
+});
 
 /*get home page*/
 router.get('/', (req, res, next) => {
-    res.render('home');
+    homeData.find() 
+        .then(function (doc) {
+            var copy = cloneToObjArr(doc);
+            res.render('home', {homeFields: copy});
+        });
 });
 
 router.get('/home', (req, res, next) => {
-    res.render('home');
-});
-
-router.get('/cv', (req, res, next) => {
-    res.render('cv');
+    homeData.find() 
+        .then(function (doc) {
+            var copy = cloneToObjArr(doc);
+            res.render('home', {homeFields: copy});
+        });
 });
 
 router.get('/hobbies', (req, res, next) => {
-    res.render('hobbies');
+    hbData.find() 
+        .then(function (doc) {
+            var copy = cloneToObjArr(doc);
+            res.render('hobbies', {hbFields: copy});
+        });
 });
 
-/*
-
-router.get('/hobbys', (req, res, next) => {
-    res.render('hobbies');
-});
-
-
-router.get('/cv', (req, res, next) => {
-    res.render('cv');
-});
-*/
 module.exports = router;
--- withNode/server.js
+++ withNode/server.js
@@ -5,21 +5,41 @@ const PORT = process.env.PORT || 5000;
 const express = require('express');
 const hbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const favicon = require('serve-favicon');
 
 const app = express();
 
 //view engine setup
 var layoutsPath = path.join(__dirname, path.join('views', 'layouts'));
-console.log(layoutsPath);
-
 app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'main', layoutsDir: layoutsPath}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+var hbsC = hbs.create({});
+hbsC.handlebars.registerHelper('setColor', function (index) {
+    if (parseInt(index) % 2 === 0){
+        return '#eee';
+    }
+    return '#aaa';
+});
+// hbsC.handlebars.registerHelper('imgOrder', function (index) {
+//     if (parseInt(index) % 2 === 0){
+//         return `<h1>{{title}}</h1>
+//                 <p></p>
+//                 {{#each content}}
+//                     <p>{{this}}</p>
+//                 {{/each}}`;
+//     }
+//     return `<img src="{{imgURL}}" class="img-fluid " style="border-radius: 15px;">`;
+// });
 
 //other
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use(favicon(path.join(__dirname,'public','favicon.ico.png')));
+app.use('/favicon.ico', express.static('public/favicon.ico.png'));
 
 //routing
 app.use('/', routes);
